499beb4f8f6f826e8eab40f75c452502a82f6e18
oladeji anthony <oladejianthony98@gmail.com>
2024-05-29 18:19:43 +0100

    login page

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,59 +1,103 @@
+// Import the mongoose module
 const mongoose = require('mongoose');
 
+// Define the schema for a User
 const userSchema = new mongoose.Schema({
-   name:{
-       type:String,
-       required:true,
+   // Define the 'name' field
+   name: {
+       type: String, // The type of data is a string
+       required: true, // This field is required
    },
-   email:{
-       type:String,
-       required:true,
-       unique:true,
+   // Define the 'email' field
+   email: {
+       type: String, // The type of data is a string
+       required: true, // This field is required
+       unique: true, // This field must be unique (no duplicate emails)
    },
-   password:{
-       type:String,
-       required:true,
+   // Define the 'password' field
+   password: {
+       type: String, // The type of data is a string
+       required: true, // This field is required
    },
-   verified:{
-       type:Boolean,
-       default:false
+   // Define the 'verified' field
+   verified: {
+       type: Boolean, // The type of data is a boolean (true or false)
+       default: false // The default value is false
    },
-   verificationToken:String,
-   profileImage:String,
-   userDescription:{
-       type:String,
-       default:null
+   // Define the 'verificationToken' field
+   verificationToken: String, // The type of data is a string
+   // Define the 'profileImage' field
+   profileImage: String, // The type of data is a string
+   // Define the 'userDescription' field
+   userDescription: {
+       type: String, // The type of data is a string
+       default: null // The default value is null
    },
-   connections:[
+   // Define the 'connections' field (an array of user IDs)
+   connections: [
        {
-           type:mongoose.Schema.Types.ObjectId,
-           ref:"User"
+           type: mongoose.Schema.Types.ObjectId, // The type of data is an ObjectId (references another document)
+           ref: "User" // The referenced model is "User"
        }
    ],
-   connectionRequests:[
+   // Define the 'connectionRequests' field (an array of user IDs)
+   connectionRequests: [
        {
-           type:mongoose.Schema.Types.ObjectId,
-           ref:"User"
+           type: mongoose.Schema.Types.ObjectId, // The type of data is an ObjectId (references another document)
+           ref: "User" // The referenced model is "User"
        }
    ],
-   sentConnectionRequests:[
+   // Define the 'sentConnectionRequests' field (an array of user IDs)
+   sentConnectionRequests: [
        {
-           type:mongoose.Schema.Types.ObjectId,
-           ref:"User"
+           type: mongoose.Schema.Types.ObjectId, // The type of data is an ObjectId (references another document)
+           ref: "User" // The referenced model is "User"
        }
    ],
-   posts:[
+   // Define the 'posts' field (an array of post IDs)
+   posts: [
        {
-           type:mongoose.Schema.Types.ObjectId,
-           ref:"Post"
+           type: mongoose.Schema.Types.ObjectId, // The type of data is an ObjectId (references another document)
+           ref: "Post" // The referenced model is "Post"
        }
    ],
-   createdAt:{
-       type:Date,
-       default:Date.now
+   // Define the 'createdAt' field
+   createdAt: {
+       type: Date, // The type of data is a date
+       default: Date.now // The default value is the current date and time
    }
 });
 
-const User = mongoose.model("User",userSchema);
+// Create a model for User using the userSchema
+const User = mongoose.model("User", userSchema);
 
+// Export the User model so it can be used in other parts of the application
 module.exports = User;
+
+
+//EXLANATIONS:
+/*
+Defining Schema - 
+`const userSchema = new mongoose.Schema({...});`: This line creates a new schema for the User model. A schema defines the structure of documents in the MongoDB collection, including fields and their types.
+
+*/
+/*
+Schema Fields:
+name: A required string field for the user's name.
+email: A required and unique string field for the user's email.
+password: A required string field for the user's password.
+verified: A boolean field indicating whether the user is verified, with a default value of false.
+verificationToken: A string field for storing a token used to verify the user.
+profileImage: A string field for storing the URL of the user's profile image.
+userDescription: A string field for a description of the user, with a default value of null.
+connections: An array of ObjectIds referencing other User documents, representing the user's connections.
+connectionRequests: An array of ObjectIds referencing other User documents, representing incoming connection requests.
+sentConnectionRequests: An array of ObjectIds referencing other User documents, representing sent connection requests.
+posts: An array of ObjectIds referencing Post documents, representing the user's posts.
+createdAt: A date field indicating when the user was created, with a default value of the current date and time.
+*/
+/*
+Creating A Model -
+const User = mongoose.model("User", userSchema);: This line creates a model named User using the userSchema. A model is a constructor compiled from the schema and is used to create and read documents from the underlying MongoDB collection.
+
+*/
--- frontend/app/index.js
+++ frontend/app/index.js
@@ -2,9 +2,10 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Redirect } from 'expo-router'
 
-
+//This is now our Root file
 const index = () => {
   return (
+     // Redirect to the '/(authenticate)/login' route
    <Redirect href="/(authenticate)/login"/>
   )
 }
@@ -12,6 +13,10 @@ const index = () => {
 export default index
 
 const styles = StyleSheet.create({})
+
+/*
+<Redirect href="/(authenticate)/login"/>: Inside the return statement of the functional component, the Redirect component is used to navigate the user to the /(authenticate)/login route. This means that when this component is rendered, the user will be immediately redirected to the login page.
+*/
 
 //Home(initial) Route because we are using Expo-Router
 //index.js => "/"
